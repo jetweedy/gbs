882c7c96459e8bd8b692d34d8b4f5966b3dadd7a
Jon Tweedy <jetweedy@gmail.com>
2019-05-04 14:37:27 +0000

    init commit

---
+++ src/API.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function API() {
+	return (
+		<p>Api is now working!</p>
+	);
+}
+
+export default API;
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import API from './API.js'
+
+function App() {
+	return (
+		<API />
+	);
+}
+
+export default App;
